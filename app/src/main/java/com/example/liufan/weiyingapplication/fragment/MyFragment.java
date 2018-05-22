6ea7ca7098575374b6f7f067969cec08464fbf50
liutian941013 <811308544@qq.com>
2018-05-22 10:09:22 +0800

    lh

--- app/src/main/java/com/example/liufan/weiyingapplication/fragment/MyFragment.java
+++ app/src/main/java/com/example/liufan/weiyingapplication/fragment/MyFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.liufan.weiyingapplication.R;
@@ -16,10 +18,19 @@ import com.example.liufan.weiyingapplication.R;
  */
 
 public class MyFragment  extends Fragment {
+
+    private ImageView img_setting;
+    private ListView my_list;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
          View inflate = View.inflate(getActivity(), R.layout.myfragment, null);
+         //设置的控件
+        img_setting = inflate.findViewById(R.id.img_setting);
+        //listView
+        my_list = inflate.findViewById(R.id.my_list);
+
         return inflate;
     }
 }
