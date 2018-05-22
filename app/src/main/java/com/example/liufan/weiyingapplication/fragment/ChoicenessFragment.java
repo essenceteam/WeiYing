3a6c718a48dbc73c2774f02756245e6dcbf4b2e9
afffa <1413137532@qq.com>
2018-05-22 10:19:50 +0800

    sd

--- app/src/main/java/com/example/liufan/weiyingapplication/fragment/ChoicenessFragment.java
+++ app/src/main/java/com/example/liufan/weiyingapplication/fragment/ChoicenessFragment.java
@@ -4,12 +4,12 @@ package com.example.liufan.weiyingapplication.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.example.liufan.weiyingapplication.R;
 
 /**
  * Created by liufan on 2018/5/21.
@@ -19,8 +19,7 @@ public class ChoicenessFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("三生三世");
-        return textView;
+        View view = View.inflate(getActivity(), R.layout.choiceness_view, null);
+        return view;
     }
 }
